914637a70531beb13b63280b7a554166097cea11
Srijan Srivastava <srijan.srivastava@zohomail.in>
2026-06-19 19:04:18 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,11 @@
+import discord, os
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+client.run(os.environ["DISCORD_TOKEN"])
